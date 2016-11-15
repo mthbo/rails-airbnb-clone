4a7cbd938f0b748dcd7d5cbc75d3d955ca526442
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2016-11-15 21:44:16 +0100

    Few changes to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
+Deal.destroy_all
+Offer.destroy_all
+User.destroy_all
+Mean.destroy_all
+Language.destroy_all
 
 languages = [
   Language.create(name: "French"),
-  Language.create(name: "english"),
-  Language.create(name: "spanish"),
-  Language.create(name: "italian"),
-  Language.create(name: "german"),
+  Language.create(name: "English"),
+  Language.create(name: "Spanish"),
+  Language.create(name: "Italian"),
+  Language.create(name: "German"),
   Language.create(name: "Syrian"),
   Language.create(name: "Créole")
 ]
@@ -19,13 +24,13 @@ means = [
 
 10.times do
   User.create(
-    first_name: Faker::Name.name,
+    first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     password:  Faker::Internet.password,
     phone_number: Faker::PhoneNumber.cell_phone,
     email: Faker::Internet.email,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    bio: Faker::Lorem.sentence,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.zip} #{Faker::Address.city}, #{Faker::Address.country}",
+    bio: Faker::Lorem.paragraph(20),
     birth_date: Faker::Date.between(60.years.ago, Date.today)
   )
 end
@@ -33,7 +38,7 @@ end
 10.times do
   Offer.create(
     title: Faker::Name.title,
-    description: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph(20),
     advisor: User.find(rand(1..10)),
     languages: languages.sample(rand(1..4)),
     means: means.sample(rand(1..5))
@@ -42,59 +47,55 @@ end
 
 5.times do
   Deal.create(
-    request: Faker::Lorem.sentence,
-    deadline: Faker::Date.backward(14),
-    price:  Faker::Commerce.price,
-    proposition: Faker::Lorem.sentence,
-    proposition_at: Faker::Date.backward(34),
-    accepted_at: Faker::Date.backward(24),
-    closed_at: Faker::Date.backward(14),
-    advisor_review: Faker::Lorem.sentence,
-    client_review: Faker::Lorem.sentence,
-    client_rating: (0..5).to_a.sample,
-    advisor_rating: (0..5).to_a.sample,
     offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10))
+    client: User.find(rand(1..10)),
+    request: Faker::Lorem.paragraph(10),
+    deadline: Faker::Time.between(0.days.ago, 1.day.ago),
+    proposition: Faker::Lorem.paragraph(10),
+    price:  Faker::Commerce.price,
+    proposition_at: Faker::Time.between(5.days.ago, 6.days.ago),
+    accepted_at: Faker::Time.between(3.days.ago, 4.days.ago),
+    closed_at: Faker::Time.between(1.day.ago, 2.days.ago),
+    client_review: Faker::Lorem.paragraph(10),
+    advisor_review: Faker::Lorem.paragraph(10),
+    client_rating: (0..5).to_a.sample,
+    advisor_rating: (0..5).to_a.sample
   )
 end
 
 
 5.times do
   Deal.create(
-    request: Faker::Lorem.sentence,
+    offer: Offer.find(rand(1..10)),
+    client: User.find(rand(1..10)),
+    request: Faker::Lorem.paragraph(10),
     deadline: Faker::Date.forward(14),
     price:  Faker::Commerce.price,
-    proposition: Faker::Lorem.sentence,
-    proposition_at: Faker::Date.backward(24),
-    accepted_at: Faker::Date.backward(14),
-    offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10))
+    proposition: Faker::Lorem.paragraph(10),
+    proposition_at: Faker::Time.between(3.days.ago, 4.days.ago),
+    accepted_at: Faker::Time.between(1.day.ago, 2.days.ago)
   )
 end
 
 5.times do
   Deal.create(
-    request: Faker::Lorem.sentence,
+    offer: Offer.find(rand(1..10)),
+    client: User.find(rand(1..10)),
+    request: Faker::Lorem.paragraph(10),
     deadline: Faker::Date.forward(24),
     price:  Faker::Commerce.price,
-    proposition: Faker::Lorem.sentence,
-    proposition_at: Faker::Date.backward(14),
-    offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10))
+    proposition: Faker::Lorem.paragraph(10),
+    proposition_at: Faker::Time.between(1.day.ago, 2.days.ago)
   )
 end
 
 5.times do
   Deal.create(
-    request: Faker::Lorem.sentence,
-    deadline: Faker::Date.forward(34),
     offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10))
+    client: User.find(rand(1..10)),
+    request: Faker::Lorem.paragraph(10),
+    deadline: Faker::Date.forward(34)
   )
 end
-
-
-
-puts "It's OK"
 
 
