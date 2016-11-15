8b45d1f373bf4f613b36aef75e412086ac90998a
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2016-11-16 00:02:46 +0100

    offer & user model & views updated

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -8,4 +8,39 @@ class Offer < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+
+  def global_rating
+    unless deals_reviewed.count.zero?
+      sum = 0
+      deals_reviewed.each { |deal| sum += deal.client_rating }
+      sum.fdiv(deals_reviewed.count)
+    end
+  end
+
+  def min_price
+    deals_closed.minimum(:price)
+  end
+
+  def max_price
+    deals_closed.maximum(:price)
+  end
+
+  def median_price
+    unless deals_closed.count.zero?
+      prices = []
+      deals_closed.each { |deal| prices << deal.price }
+      prices.sort!
+      len = prices.length
+      (prices[(len - 1) / 2] + prices[len / 2]) / 2
+    end
+  end
+
+  def deals_closed
+    deals.where.not(closed_at: nil)
+  end
+
+  def deals_reviewed
+    deals.where.not(client_review: nil)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   has_many :deals, foreign_key: 'client_id'
   has_many :advisor_deals, through: :offers, source: :deals
 
+  def grade
+    if advisor_deals.count > 20
+      return "Champion"
+    elsif advisor_deals.count > 10
+      return "Senior Advisor"
+    elsif advisor_deals.count > 3
+      return "Advisor"
+    else
+      return "Roockie"
+    end
+  end
+
 end
