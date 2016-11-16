fa9369c201a15d8c19a387c268e46048bb27d7e2
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2016-11-16 14:29:45 +0100

    User#show + users_controller + few modification to models and views

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,9 +1,9 @@
 class Offer < ApplicationRecord
   belongs_to :advisor, class_name: 'User'
   has_many :deals
-  has_many :offer_means
+  has_many :offer_means, dependent: :destroy
   has_many :means, through: :offer_means
-  has_many :offer_languages
+  has_many :offer_languages, dependent: :destroy
   has_many :languages, through: :offer_languages
 
   validates :title, presence: true
