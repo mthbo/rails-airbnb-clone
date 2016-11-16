17638545adc755d5d85abc06cc2c7c32df260830
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2016-11-16 17:53:17 +0100

    seed modif 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,9 @@ means = [
   Mean.create(name: "Date")
 ]
 
+users = []
 10.times do
-
-  user = User.new(
+  users << User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     password:  Faker::Internet.password,
@@ -34,25 +34,24 @@ means = [
     bio: Faker::Lorem.paragraph(20),
     birth_date: Faker::Date.between(60.years.ago, Date.today)
   )
-  user.save!
-
 end
 
+offers = []
 10.times do
-  offer = Offer.new(
+  offers << Offer.create(
     title: Faker::Name.title,
     description: Faker::Lorem.paragraph(20),
-    advisor: User.find(rand(1..10)),
+    advisor: users.sample,
     languages: languages.sample(rand(1..4)),
     means: means.sample(rand(1..5))
   )
-  offer.save!
 end
 
+deals1 =[]
 5.times do
-  deal = Deal.new(
-    offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10)),
+  deals1 << Deal.create(
+    offer: offers.sample,
+    client: users.sample,
     request: Faker::Lorem.paragraph(10),
     deadline: Faker::Time.between(0.days.ago, 1.day.ago),
     proposition: Faker::Lorem.paragraph(10),
@@ -65,14 +64,13 @@ end
     client_rating: (0..5).to_a.sample,
     advisor_rating: (0..5).to_a.sample
   )
-  deal.save!
 end
 
-
+deals2 = []
 5.times do
-  deal = Deal.new(
-    offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10)),
+  deals2 << Deal.create(
+    offer: offers.sample,
+    client: users.sample,
     request: Faker::Lorem.paragraph(10),
     deadline: Faker::Date.forward(14),
     price:  Faker::Commerce.price,
@@ -80,30 +78,29 @@ end
     proposition_at: Faker::Time.between(3.days.ago, 4.days.ago),
     accepted_at: Faker::Time.between(1.day.ago, 2.days.ago)
   )
-  deal.save!
 end
 
+deals3 = []
 5.times do
-  deal = Deal.new(
-    offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10)),
+  deals3 << Deal.create(
+    offer: offers.sample,
+    client: users.sample,
     request: Faker::Lorem.paragraph(10),
     deadline: Faker::Date.forward(24),
     price:  Faker::Commerce.price,
     proposition: Faker::Lorem.paragraph(10),
     proposition_at: Faker::Time.between(1.day.ago, 2.days.ago)
   )
-  deal.save!
 end
 
+deals4 = []
 5.times do
-  deal = Deal.new(
-    offer: Offer.find(rand(1..10)),
-    client: User.find(rand(1..10)),
+  deals4 << Deal.create(
+    offer: offers.sample,
+    client: users.sample,
     request: Faker::Lorem.paragraph(10),
     deadline: Faker::Date.forward(34)
   )
-  deal.save!
 end
 
 urls = [
@@ -118,20 +115,6 @@ urls = [
   'http://lorempixel.com/100/100/people/9/',
   'http://lorempixel.com/100/100/people/10/'
 ]
-
-
-user1 = User.find(1)
-user2 = User.find(2)
-user3 = User.find(3)
-user4 = User.find(4)
-user5 = User.find(5)
-user6 = User.find(6)
-user7 = User.find(7)
-user8 = User.find(8)
-user9 = User.find(9)
-user10 = User.find(10)
-
-users = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10]
 
 users.each_with_index do |user, index|
   user.photo_url = urls[index]
