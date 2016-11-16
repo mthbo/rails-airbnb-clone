0b3e1871aa12124339d75e1d455de55898812c98
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2016-11-16 12:33:43 +0100

    first step with authentification + user model modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,23 @@ class User < ApplicationRecord
   has_many :advisor_deals, through: :offers, source: :deals
 
   def grade
-    if advisor_deals.count > 20
+    if advisor_deals_closed.count > 20
       return "Champion"
-    elsif advisor_deals.count > 10
+    elsif advisor_deals_closed.count > 10
       return "Senior Advisor"
-    elsif advisor_deals.count > 3
+    elsif advisor_deals_closed.count > 3
       return "Advisor"
     else
       return "Roockie"
     end
+  end
+
+  def advisor_deals_closed
+    advisor_deals.where.not(closed_at: nil)
+  end
+
+  def advisor_deals_reviewed
+    advisor_deals.where.not(client_review: nil)
   end
 
 end
