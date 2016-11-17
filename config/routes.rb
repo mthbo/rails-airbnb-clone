eb65368324c5bbcf10da3030d778fd3a2325d48a
hatchi julien <julien.hatchi@gmail.com>
2016-11-17 16:22:40 +0100

    facebook connect

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
 
   resources :offers do
