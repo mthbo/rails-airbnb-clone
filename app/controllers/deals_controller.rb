34ef265c1f26910d534d52d528450909e33f6ba3
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2016-11-17 15:14:23 +0100

    Deals routes / controller / model / views, not finished

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,5 +1,6 @@
 class DealsController < ApplicationController
-  before_action :find_deal, only: [:show, :edit, :update, :destroy]
+  before_action :find_deal, only: [:show]
+  before_action :find_offer, only: [:create, :new, :show]
 
   def show
   end
@@ -8,16 +9,17 @@ class DealsController < ApplicationController
     @deal = Deal.new
   end
 
-#   def create
-#     @deal = current_user.offers.new(offer_params)
-#     if @offer.save
-#       flash[:notice] = "#{@offer.title} has been created"
-#       redirect_to offer_path(@offer)
-#     else
-#       flash[:alert] = "No offer has been created"
-#       render :new
-#     end
-#   end
+  def create
+    @deal = @offer.deals.new(deal_params)
+    @deal.client = current_user
+    if @deal.save
+      flash[:notice] = "A request has been sent to #{@offer.advisor.name_anonymous} regarding the offer '#{@offer.title}'"
+      redirect_to offer_path(@offer)
+    else
+      flash[:alert] = "No request has been sent"
+      render :new
+    end
+  end
 
 #   def edit
 #   end
@@ -32,23 +34,18 @@ class DealsController < ApplicationController
 #     end
 #   end
 
-#   def destroy
-#     @offer.destroy
-#     redirect_to user
-#   end
+  private
 
-#   private
+  def find_deal
+    @deal = Deal.find(params[:id])
+  end
 
-#   def find_deal
-#     @deal = Deal.find(params[:id])
-#   end
+  def find_offer
+    @offer = Offer.find(params[:offer_id])
+  end
 
-#   def find_offer
-#     @offer = Offer.find(params[:id])
-#   end
-
-#   def offer_params
-#     params.require(:offer).permit(:title, :description)
-#   end
+  def deal_params
+    params.require(:deal).permit(:request, :deadline)
+  end
 
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -7,4 +7,28 @@ class Deal < ApplicationRecord
   def advisor
     self.offer.advisor
   end
+
+  def status
+    if !closed_at.nil?
+      "Closed"
+    elsif !accepted_at.nil?
+      "Open"
+    elsif !proposition_at.nil?
+      "Proposition"
+    else
+      "Request"
+    end
+  end
+
+  def proposition?
+    !proposition_at.nil?
+  end
+
+  def accepted?
+    !accepted_at.nil?
+  end
+
+  def closed?
+    !closed_at.nil?
+  end
 end
