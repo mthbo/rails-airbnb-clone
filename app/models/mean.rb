79e5db918ccd10bd3592ba74db0b8e0ee44e75fa
hatchi julien <julien.hatchi@gmail.com>
2016-11-18 17:32:19 +0100

    style offer-cards: done

--- app/models/mean.rb
+++ app/models/mean.rb
@@ -1,4 +1,17 @@
 class Mean < ApplicationRecord
   has_many :offer_means
   has_many :offers, through: :offer_means
+
+  PICTO = {
+      "SMS" => 'fa-mobile fa-lg',
+      "Visio-call" => 'fa-video-camera fa-lg',
+      "Call" => 'fa-phone fa-lg',
+      "Email" => 'fa-envelope fa-lg',
+      "Meeting" => 'fa-comments fa-lg',
+    }
+
+
+  def picto
+    PICTO[self.name]
+  end
 end
