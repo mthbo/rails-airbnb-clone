ab96bcfbd8050ab5ccf7329a1d65d29774b39a67
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-01-13 19:06:35 +0100

    heroku action cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # WebSocket
+  config.web_socket_server_url = "wss://papoters.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://papoters.herokuapp.com', 'http://papoters.herokuapp.com', 'http://www.papoters.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
