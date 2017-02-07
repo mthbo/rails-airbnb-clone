d4397f3b2b9dd13aa7bfd844aaf7661a99b1b617
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-02-07 17:51:08 +0100

    navbar qq

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,8 @@
+$(function() {
+
+  $('#mobile-dropdown').on('show.bs.dropdown', function () {
+    $('.my-navbar').removeClass("my-navbar-scrolled");
+  })
+
+
+});
--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -7,11 +7,9 @@ if (($('.my-navbar').length > 0) && ($('.home').length > 0)) {
 function checkScroll(){
   var startY = $('.my-navbar').height() * 2;
 
-  if($(window).scrollTop() < startY) {
+  if($(window).scrollTop() < startY && $('.mobile-dropdown-menu').is(":hidden")) {
     $('.my-navbar').addClass("my-navbar-scrolled");
-    $('#my-navbar-search').hide();
   } else {
     $('.my-navbar').removeClass("my-navbar-scrolled");
-    $('#my-navbar-search').show();
   }
 };
