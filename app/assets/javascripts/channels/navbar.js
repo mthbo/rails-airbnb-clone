f1c1cd0543bb10f42474a0bfb2d1aa623a775c1b
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-02-10 17:47:39 +0100

    footer design + front modif

--- app/assets/javascripts/channels/navbar.js
+++ app/assets/javascripts/channels/navbar.js
@@ -14,6 +14,8 @@ function subscribeToUserNotificationsChannel() {
   App['user:notifications'] = App.cable.subscriptions.create({channel: 'UserNotificationsChannel'}, {
     received: function(data) {
       $('.my-navbar-badge').html(data.notifications);
+      $('.my-navbar-mobile-dropdown-badge').html(data.notifications);
+      $('.my-navbar-mobile-menu-badge').html(data.notifications);
     }
   });
 };
