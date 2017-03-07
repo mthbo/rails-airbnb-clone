e55d2c18d9b733844c61289357648829c67ab5dd
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-03-07 18:41:58 +0100

    homepage banner and inspirations

--- app/assets/javascripts/arrow-scroll.js
+++ app/assets/javascripts/arrow-scroll.js
@@ -2,7 +2,7 @@ $(function(){
 
   $('#home-arrow-down').on('click', function(event) {
     event.preventDefault();
-    window.scroll({ top: $('.home-banner').height(), left: 0, behavior: 'smooth' });
+    window.scroll({ top: $('#home-banner').height(), left: 0, behavior: 'smooth' });
   });
 
 });
