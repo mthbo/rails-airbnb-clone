b8de7c8449e9246b39aa6ea318657befa744b9d6
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-03-31 13:56:56 +0200

    translation for session, first message and dating messages

--- app/models/mean.rb
+++ app/models/mean.rb
@@ -21,8 +21,12 @@ class Mean < ApplicationRecord
     PICTOS[self.name]
   end
 
+  def picto_i
+    "<i class='fa fa-#{self.picto} fa-fw fa-lg' aria-hidden='true'></i>".html_safe
+  end
+
   def name_illustrated(locale=I18n.locale)
-    "<span class='mean-icon mean-icon-long'><i class='fa fa-#{self.picto} fa-fw fa-lg' aria-hidden='true'></i> #{self.name_translated(locale)}</span>".html_safe
+    "<span class='mean-icon mean-icon-long'>".html_safe + picto_i + "#{self.name_translated(locale)}</span>".html_safe
   end
 
   def offers_count
--- app/models/message.rb
+++ app/models/message.rb
@@ -9,39 +9,37 @@ class Message < ApplicationRecord
   after_create_commit :async_message_broadcast
 
   def build_first_content
-    language_names = self.deal.languages.map { |language| language.name }
-    languages_list = "<span class='font-weight-bolder'>Languages accepted:</span> #{language_names.join(', ')}"
-    mean_names = self.deal.means.map { |mean| mean.name }
-    means_list = "<span class='font-weight-bolder'>Means accepted:</span> #{mean_names.join(', ')}"
-    deadline_format = "<span class='font-weight-bolder'>Deadline:</span> #{self.deal.deadline.strftime('%d %b %Y')}"
-    self.content = "#{self.deal.request} <br> #{languages_list} <br> #{means_list} <br> #{deadline_format}"
+    deadline_format = "<span class='message-deadline'><span class='info-icon'>#{ActionController::Base.helpers.image_tag('date.svg')}</span><span class='font-weight-normal'>#{I18n.l(self.deal.deadline.to_date, format: :default)}</span></span>"
+    language_flags = self.deal.languages.map{ |language| "<span class='flag-icon-message'>#{language.flag_img}</span>" }.join
+    mean_pictos = self.deal.means.map { |mean| "<span class='mean-icon-message'>#{mean.picto_i}</span>" }.join
+    self.content = "#{deadline_format} #{self.deal.request} <br> #{language_flags} <br> #{mean_pictos}"
   end
 
   def build_deal_status_content
     if self.deal.proposition?
-      self.content = "<span class='font-weight-normal blue'>New proposition </span>"
+      self.content = "<span class='font-weight-normal blue'>#{I18n.t(message.new_proposition)}</span>"
     elsif self.deal.proposition_declined?
-      self.content = "<span class='font-weight-normal blue'>Proposition declined</span>"
+      self.content = "<span class='font-weight-normal blue'>#{I18n.t(message.proposition_declined)}</span>"
     elsif self.deal.open?
-      self.content = "<span class='font-weight-normal blue'>Session open</span>"
+      self.content = "<span class='font-weight-normal blue'>#{I18n.t(message.session_open)}</span>"
     elsif self.deal.open_expired?
-      self.content = "<span class='font-weight-normal blue'>Session deadline passed</span>"
+      self.content = "<span class='font-weight-normal blue'>#{I18n.t(message.session_deadline_passed)}</span>"
     elsif self.deal.closed?
-      self.content = "<span class='font-weight-normal red'>Session closed</span>"
+      self.content = "<span class='font-weight-normal red'>#{I18n.t(message.session_closed)}</span>"
     elsif self.deal.cancelled?
-      self.content = "<span class='font-weight-normal red'>Session cancelled</span>"
+      self.content = "<span class='font-weight-normal red'>#{I18n.t(message.session_cancelled)}</span>"
     end
   end
 
   def date_formatted
     if created_at.year < DateTime.current.in_time_zone.year
-      created_at.strftime('%d %b %Y')
+      I18n.l(created_at.to_date, format: :long)
     elsif created_at < 6.days.ago.beginning_of_day
-      created_at.strftime('%d %b')
+      I18n.l(created_at.to_date, format: :short)
     elsif created_at < DateTime.current.in_time_zone.beginning_of_day
-      created_at.strftime('%A')
+      I18n.l(created_at.to_date, format: "%A")
     else
-      created_at.strftime('%H:%M')
+      I18n.l(created_at, format: "%H:%M")
     end
   end
 
