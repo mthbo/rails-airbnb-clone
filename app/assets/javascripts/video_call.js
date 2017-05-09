ab2736eb0c36f0dd3457506a0c96d2bd68f73114
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-05-09 16:02:14 +0200

    video call trigger appear.in

--- app/assets/javascripts/video_call.js
+++ app/assets/javascripts/video_call.js
@@ -17,16 +17,16 @@ $(document).ready(function() {
 
   // add the room to an iframe DOM Element
 
-  var $videoCall = $("#video-call");
+  // var $videoCall = $("#video-call");
 
-  if ($videoCall.length > 0) {
-    var AppearIn = window.AppearIn;
-    var appearin = new AppearIn();
+  // if ($videoCall.length > 0) {
+  //   var AppearIn = window.AppearIn;
+  //   var appearin = new AppearIn();
 
-    var roomName = $videoCall.attr('data-room-name');
-    $("#video-call-modal").on('shown.bs.modal', function () {
-      appearin.addRoomToElementById("video-call-frame", roomName);
-    });
-  }
+  //   var roomName = $videoCall.attr('data-room-name');
+  //   $("#video-call-modal").on('shown.bs.modal', function () {
+  //     appearin.addRoomToElementById("video-call-frame", roomName);
+  //   });
+  // }
 
 });
