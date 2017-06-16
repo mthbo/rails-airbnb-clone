433940d1475d650c0f787aa6821e8db1cafecef0
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-06-16 19:35:04 +0200

    postmark transition

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
   config.action_mailer.default_url_options = { host: ENV['APP_DOMAIN'], protocol: "https" }
   config.action_mailer.asset_host = "https://#{ENV['APP_DOMAIN']}"
 
