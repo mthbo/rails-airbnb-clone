652ea71a62b820ee87b8b2eeba89d819f1e6401a
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-08-01 18:34:37 +0200

    advisor landing new version

--- app/assets/javascripts/arrow_scroll.js
+++ app/assets/javascripts/arrow_scroll.js
@@ -2,7 +2,7 @@ $(function(){
 
   $('#home-arrow-down').on('click', function(event) {
     event.preventDefault();
-    window.scroll({ top: $('.home-banner').height(), left: 0, behavior: 'smooth' });
+    window.scroll({ top: window.innerHeight, left: 0, behavior: 'smooth' });
   });
 
 });
