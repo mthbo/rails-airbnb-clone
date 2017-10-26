5bac079d2f39e8886b8a6c4f071b6b6cbd0ebd0d
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-10-26 11:51:49 +0200

    title notifications

--- app/assets/javascripts/channels/navbar.js
+++ app/assets/javascripts/channels/navbar.js
@@ -4,18 +4,32 @@ $(document).ready(function() {
 
   if ( $navbarLogged.length > 0 ) {
     var currentUserId = parseInt($navbarLogged.attr('data-user-id'));
-    subscribeToUserNotificationsChannel();
+    var title = document.title;
+    updateTitleNotifications(title);
+    subscribeToUserNotificationsChannel(title);
   }
 
 });
 
 
-function subscribeToUserNotificationsChannel() {
+function subscribeToUserNotificationsChannel(title) {
   App['user:notifications'] = App.cable.subscriptions.create({channel: 'UserNotificationsChannel'}, {
     received: function(data) {
       $('#my-navbar-badge').html(data.notifications);
       $('#my-navbar-burger-badge').html(data.notifications);
       $('#my-navbar-burger-menu-badge').html(data.notifications);
+      updateTitleNotifications(title);
     }
   });
 };
+
+function updateTitleNotifications(title) {
+  var notificationCount = parseInt($('#user-notifications').text());
+  console.log(notificationCount);
+  if (notificationCount !== 0) {
+    var newTitle = '(' + notificationCount + ') ' + title;
+    document.title = newTitle;
+  } else {
+    document.title = title;
+  }
+}
