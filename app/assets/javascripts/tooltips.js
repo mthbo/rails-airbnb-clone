3d94453541b140bf67551f48584988904c0c2819
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-10-29 12:13:56 +0100

    dashboard btn tooltip

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 
   var $videoCallBtns = $('#video-call-btn');
+  var $dashboardBtn = $('#my-navbar-dashboard-btn');
 
   if (!isMobile()) {
     $videoCallBtns.tooltip({
+      trigger: 'hover',
+      placement: 'bottom'
+    });
+
+    $dashboardBtn.tooltip({
       trigger: 'hover',
       placement: 'bottom'
     });
