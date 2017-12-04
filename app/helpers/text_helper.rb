76df78aea1d1ae152b675b4ccebc4995a824704a
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2017-12-04 11:46:49 +0100

    kramdown config

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -2,6 +2,6 @@ module TextHelper
 
   # Returns HTML from input text using GitHub-flavored Markdown.
   def markdown_to_html(text)
-    Kramdown::Document.new(text, input: 'GFM').to_html
+    Kramdown::Document.new(text, {input: 'GFM', header_offset: 5}).to_html
   end
 end
